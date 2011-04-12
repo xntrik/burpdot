1f6077a0f92233f4062bc6d4e68b0c0d9aa5d609
Christian Frichot <xntrik@gmail.com>
2011-04-12 17:08:52 +0800

    working on web processing the burp log file

--- lib/web/burpfileupload.rb
+++ lib/web/burpfileupload.rb
@@ -1,14 +1,25 @@
+require 'lib/import'
+require 'pp'
+
 module Burpdot
   
 class WebBurpFileUpload < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(req, res)
     filedata = req.query["logfile"]
     
-    f = File.open("foo.out", "wb")
-    f.syswrite filedata
-    f.close
+    #f = File.open("foo.out", "wb")
+    #f.syswrite filedata
+    #f.close
+
+    options = {}
+    options['depth'] = 2
+    options['input'] = filedata.split("\n")
+    output = Burpdot::Import.importburpbase(options)
+
+    res.body = pp output
+    raise WEBrick::HTTPStatus::OK    
+    #res.set_redirect(WEBrick::HTTPStatus::Found,"/")
     
-    res.set_redirect(WEBrick::HTTPStatus::Found,"/")
   end
 end
 
