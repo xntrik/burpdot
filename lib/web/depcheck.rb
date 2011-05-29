f4f1f1e3bbedd6b9335637e2da28751ed1f116da
Christian Frichot <xntrik+git@gmail.com>
2011-05-29 14:40:25 +0800

    Updated dep checker

--- lib/web/depcheck.rb
+++ lib/web/depcheck.rb
@@ -3,8 +3,28 @@ module Burpdot
   class Depcheck
     
     def checkdeps
-
-      exit
+      @gems_required = ["parseconfig","dm-core","dm-migrations","dm-aggregates","dm-sqlite-adapter","builder"]
+      @gems_missing = Array.new
+      
+      @gems_required.each do |current_gem|
+        begin
+          gem current_gem
+        rescue Gem::LoadError
+          @gems_missing << current_gem
+        end
+      end
+      
+      if @gems_missing.length == 0 
+        #No missing gems
+        return true
+      else
+        #You're missing gems
+        puts "You're missing some gems, make sure you install the following gems:"
+        @gems_missing.each do |current_gem|
+          puts current_gem
+        end
+        exit
+      end
     end
   end
 
