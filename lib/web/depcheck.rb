108fe0c39fa989f0adbf5d1b8a9b1783b08b1cf6
Christian Frichot <xntrik+git@gmail.com>
2011-05-29 15:00:06 +0800

    Depcheck now checks for afterglow

--- lib/web/depcheck.rb
+++ lib/web/depcheck.rb
@@ -2,6 +2,14 @@ module Burpdot
 
   class Depcheck
     
+    def checkafterglow
+      if File.exists?("afterglow.pl")
+        return true
+      else
+        return false
+      end
+    end
+    
     def checkdeps
       @gems_required = ["parseconfig","dm-core","dm-migrations","dm-aggregates","dm-sqlite-adapter","builder"]
       @gems_missing = Array.new
@@ -16,12 +24,20 @@ module Burpdot
       
       if @gems_missing.length == 0 
         #No missing gems
-        return true
+        if !checkafterglow
+          puts "You're missing Afterglow - please download Afterglow from http://afterglow.sourceforge.net/ and put afterglow.pl in Burpdot's folder"
+          puts "Ensure you've installed Perl's Text::CSV module too! (try: cpan Text::CSV)"
+          exit
+        end
       else
         #You're missing gems
         puts "You're missing some gems, make sure you install the following gems:"
         @gems_missing.each do |current_gem|
           puts current_gem
+        end
+        if !checkafterglow
+          puts "You're missing Afterglow - please download Afterglow from http://afterglow.sourceforge.net/ and put afterglow.pl in Burpdot's folder"
+          puts "Ensure you've installed Perl's Text::CSV module too! (try: cpan Text::CSV)"
         end
         exit
       end
